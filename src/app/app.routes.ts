0183625cbbcb5289bafda588d28adfd29b33328a
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-05-17 20:26:50 -0300

    feat: move to nx

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full' // para que a próxima rota seja redirecionada para a primeira
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./home/home.component').then((m) => m.HomeComponent) // -> Lazy loading
+  },
+  {
+    path: 'users',
+    loadComponent: () =>
+      import('./users/users.component').then((m) => m.UsersComponent) // -> Lazy loading
+  },
+];
